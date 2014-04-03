44161e5c492e6abfd6607c722cb3ecbcd1de2326
Amy Bakameyer <bymak87@gmail.com>
2014-04-03 22:40:56 +0900

    Keydown, Keyup
    added function when x is pressed, minor changes in html

--- js/app.js
+++ js/app.js
@@ -8,12 +8,49 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
 	})
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).bind('keydown', function(x){
+		if (x.which == 88) {
+			keyDown();
+		}
+	});
+	
+	$(document).bind('keyup', function(x){
+		if (x.which == 88) {
+			keyUp();
+		}
+	});
+		
+
 });
+function playHadouken (){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+function keyDown (){
+	$('.ryu-ready').hide();
+	$('.ryu-still').hide();
+	$('.ryu-cool').show();
+}
+
+function keyUp(){
+	$('.ryu-cool').hide();
+	$('.ryu-ready').show();
+}
